77e81cf1319684179c2e6ee7e9fe8031e5823bcf
Fran <155672163+franngo@users.noreply.github.com>
2024-08-09 19:54:51 -0300

    Create function for human choice and one round Create a function that receives and stores the pick of the human playing (user). Besides, create a function for a completely functional round of the game which involves updating the score of the winner and logging a message into the console announcing who won the round.

--- game.js
+++ game.js
@@ -1,4 +1,27 @@
-console.log("Hello!' Let's play rock, paper & scissors. Please, type your option (rock, paper or scissors).");
+let humanScore = 0;
+let computerScore = 0;
+
+console.log("Hello!' Let's play rock, paper & scissors. Please, type 1 if you wanna play.");
+//your option (rock, paper or scissors)
+//console.log(getHumanChoice());
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+playRound(humanSelection,computerSelection);
+
+function playRound(humChoice, comChoice) {
+    if(humChoice==comChoice) {
+        console.log("It's a tie!");
+        console.log("Your scores is " + humanScore + " and the computer score is " + computerScore);
+    } else if((humChoice=="rock"&&comChoice=="scissors")||(humChoice=="paper"&&comChoice=="rock")||(humChoice=="scissors"&&comChoice=="paper")) {
+        console.log("Congratulations! You won this round");
+        humanScore++;
+        console.log("Your scores is " + humanScore + " and the computer score is " + computerScore);
+    } else {
+        console.log("What a pity! You lost this round");
+        computerScore++;
+        console.log("Your scores is " + humanScore + " and the computer score is " + computerScore);
+    }
+}
 
 function getComputerChoice() {
     const random = Math.floor(Math.random()*3); //entero entre 0 y 2 incluídos
@@ -9,5 +32,18 @@ function getComputerChoice() {
             return "paper";
         default:
             return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    let answer = prompt("Type your move (rock, paper or scissors)");
+    if (answer===null) {
+        return "invalid";
+    }
+    answer = answer.toLowerCase();
+    if(!(answer=="rock"||answer=="paper"||answer=="scissors")) {
+        return "invalid";
+    } else {
+        return answer;
     }
 }
